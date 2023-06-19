5d98a4200a2c2b3ee0f742bfcc606668dfa919d2
NilaAyu1611 <nilaayucw@gmail.com>
2023-06-19 11:28:21 +0700

    menujuk file dan tampilakjn

---
+++ exceptionsatndar_138/exceptionsatndar_138.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+// untuk obyek excepton yang akan digunkana
+#include <array>
+//untuk obyek array yang kan kita gunakan 
+using namespace std;
+
+int main()
+{
+	cout << " Awal Program" << endl;			// penanda 1:...
+	try {
+		array<int, 3> data = { 1,2,3 };
+		// pesan array integer 3 elemen
+		// cout<< data.at(5)<< endl;
+		// memanggil array elemen ke 5
+	}
+	catch (exception &e) {
+		// penangkap menggunkan obyek  exeception
+		cout << e.what() << endl;
+		/*akan dieksekuis karena array data hanya memiliki 3 lemeen*/
+	}
+	cout << "Baris Program Yang terakhit" << endl;
+
+}
